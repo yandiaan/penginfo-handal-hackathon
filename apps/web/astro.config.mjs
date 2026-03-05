357e534e69dd272287e2d7a5c25722ab54266273
I Nyoman Arijaya Putra <komangajus35@gmail.com>
2026-03-05 19:08:57 +0800

    chore: Delete env file on docker compose

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -8,7 +8,14 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+  },
   vite: {
+    server: {
+      middlewareMode: true,
+      allowedHosts: ['adisai.xyz', 'www.adisai.xyz', '8.219.190.235', 'localhost'],
+    },
     plugins: [tailwindcss()],
     resolve: {
       alias: {
